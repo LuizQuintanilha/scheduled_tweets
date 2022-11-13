5eb63f8a72555349c6d2a915926ab6f5abba8c48
Luhh <luhhpersonalize@gmail.com>
2022-11-13 15:42:26 -0300

    quarto commit - flash massage

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,7 @@
 class MainController < ApplicationController
   def index
+
+    flash.now[:notice] = 'Login realizado com sucesso'
+    flash.now[:alert] = "E-mail ou senha incorretos" #.now retira o cach do navegador
   end
 end
